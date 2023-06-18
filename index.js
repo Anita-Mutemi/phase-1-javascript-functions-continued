88a415e1461f5072e838a1bb06254c96deb0ee0d
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-06-18 23:41:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // code your solution here
 // Implement a function called saturdayFun
+// Implement a function called wrapAdjective
+// Implement a function called mondayWork
 function saturdayFun(activity = 'roller-skate') {
   return `This Saturday, I want to ${activity}!`;
 }
 
-// Implement a function called wrapAdjective
 function wrapAdjective(flair = "*") {
   return function(adjective = "special") {
     return `You are ${flair}${adjective}${flair}!`;
   }
 }
 
-// Implement a function called mondayWork
 const mondayWork = function(activity = 'go to the office') {
   return `This Monday, I will ${activity}.`;
 }
