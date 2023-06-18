8af514870c279b12eec098ab904241f01430a359
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-06-18 23:39:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -15,3 +15,35 @@ function wrapAdjective(flair = "*") {
 const mondayWork = function(activity = 'go to the office') {
   return `This Monday, I will ${activity}.`;
 }
+
+// Instructions
+// To get more practice with first-class functions, this lesson has three tests to pass that require you to write the following functions in the index.js file:
+
+// The receivesAFunction function should:
+
+// take a callback function as an argument
+// call the callback function
+// it doesn't matter what this function returns, so long as it calls the callback function
+// The returnsANamedFunction function should:
+
+// take no arguments
+// return a named function
+// The returnsAnAnonymousFunction function should:
+
+// take no arguments
+// return an anonymous function
+// receivesAFunction function
+function receivesAFunction(callback) {
+  callback();
+}
+function returnsANamedFunction() {
+  function namedFunction() {
+  }
+  
+  return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+  return function() {
+  };
+}
